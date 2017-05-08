1249ef29cd80c1377bda35e4677234a95bf6a841
RobinG1ll <rsgill991@gmail.com>
2017-05-08 16:49:21 -0700

    Added server basic lines

---
+++ index.js
@@ -0,0 +1,56 @@
+//require all modules
+const express = require("express");
+const port = process.env.PORT || 10000;
+const path = require("path");
+
+const bp = require("body-parser");
+const session = require("express-session");
+
+//start server
+var app = express();
+var server = require("http").createServer(app);
+
+//Setup settings for db, server, and folders
+var pub = path.resolve(__dirname, "public");
+var io = require("socket.io")(server);
+const pg = require("pg");
+var dbURL = process.env.DATABASE_URL || "postgres://postgres:database_password@localhost:5432/database_name_here";
+
+//redirct /scripts to build folder
+app.use("/scripts", express.static("build"));
+
+//body parser setting
+app.use(bp.urlencoded({
+    extended: true
+}));
+
+//session settings
+app.use(session({
+    secret:"meow",
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.get("/", function(req, res){
+    if(req.session.username){
+        res.sendFile(pub+"/item.html");
+    }else {
+        res.sendFile(pub+"/login.html");
+    }
+});
+
+io.on("connection", function(socket){
+
+    socket.on("disconnect", function(){
+        //when the user leaves my html, they "disconnect" by cloasing the connection
+    });
+});
+
+//listen to the server and open up a port
+server.listen(port, function(err){
+    if(err){
+        console.log(err);
+        return false;
+    }
+    console.log("Server is running PORT " +port);
+});
