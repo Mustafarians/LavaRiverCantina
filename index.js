d76fae4df724e05dd1aca19423242677cff54b80
Harnoor Gill <gill_harnoor@hootmail.com>
2017-05-15 15:39:35 -0700

    udpating my files, need to commit

--- index.js
+++ index.js
@@ -16,8 +16,11 @@ var io = require("socket.io")(server);
 const pg = require("pg");
 var dbURL = process.env.DATABASE_URL || "postgres://postgres:webdev@localhost:5432/lrc";
 
-//redirct /scripts to build folder
+//redirect /scripts to build folder
 app.use("/scripts", express.static("build"));
+
+//images
+app.use("/styles", express.static("css"));
 
 //redirect /imgs to img folder
 app.use("/images", express.static("img"));
@@ -38,9 +41,23 @@ app.get("/", function(req, res){
         res.sendFile(pub+"/home.html");
 });
 
-app.get("/menu", function(req, res){
-        res.sendFile(pub+"/menu.html");
+app.get("/admin", function(req, res){
+    if(session.level == 2){
+        res.sendFile(pub+"/admin.html");
+    } else {
+        res.sendFile(pub+"/home.html");
+    }
 });
+
+app.get("/kitchen", function(req,res){
+    if(session.level == 1){
+        res.sendFile(pub+"/kitchen.html");
+    } else if (session.level == 2){
+        res.sendFile(pub+"/kitchen.html");
+    } else {
+        res.sendFile(pub+"/home.html");
+    }
+})
 
 app.get("/login", function(req, res){
     res.sendFile(pub+"/login.html");
