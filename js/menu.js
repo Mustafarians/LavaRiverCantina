e36192b60af283ec13ed25debe5137b5b3dffef9
AJayceW <AJayIce@hotmail.com>
2017-05-15 16:56:16 -0700

    Allows users to order multiple items [Finishes #144659361]

--- js/menu.js
+++ js/menu.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
     });
     
     checkBut.addEventListener("click", function(){
-        
+         
             $.ajax({
                 url:"/order66",
                 type:"post",
