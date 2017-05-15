b29dc852da0405dd8b9ca29944df8742d36e5c70
AJayceW <AJayIce@hotmail.com>
2017-05-15 13:23:57 -0700

    Removed dead code, + added order quantity

--- js/menu.js
+++ js/menu.js
@@ -3,13 +3,15 @@ $(document).ready(function(){
     var item1But = document.getElementById("item1");
    
     item1But.addEventListener("click", function(){
-        var foodname = "Tauntaun Steak"
+        var foodname = "Tauntaun Steak";
+        var quantity = document.getElementById("item1Quant").value;
         
             $.ajax({
                 url:"/order1",
                 type:"post",
                 data:{
-                    foodname:foodname
+                    foodname:foodname,
+                    quantity:quantity
                 },
                 success:function(resp){
                     if(resp.status == "success"){
